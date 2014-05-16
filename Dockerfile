ed8121cef519f600958c03d3b5efc9b8103b563d
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2014-05-16 13:27:41 -0700

    Updated the Dockerfile a bit and created password generation logic in the first run script.

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,14 @@ MAINTAINER Naftuli Tzvi Kay <rfkrocktk@gmail.com>
 
 # Ensure everything is UTF-8, because there's no reason in the world why not.
 ENV LANG en_US.UTF-8
-ENV LC_ALL en_US.UTF-8 
+#ENV LC_ALL en_US.UTF-8 # gives a warning to stderr :( 
 RUN locale-gen en_US.UTF-8
 
 # Install MariaDB PPA
 ADD conf/apt/mariadb.list /etc/apt/sources.list.d/mariadb.list
 RUN apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db \
      && apt-get update -q 2 \
-     && DEBIAN_FRONTEND=noninteractive apt-get install --yes -q 2 mariadb-server pwgen 
+     && DEBIAN_FRONTEND=noninteractive apt-get install --yes -q 2 mariadb-server sudo pwgen 
 
 # Run Initial Setup
 ADD scripts/mariadb-first-run.sh /usr/local/bin/mariadb-first-run
--- scripts/mariadb-first-run.sh
+++ scripts/mariadb-first-run.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo "MariaDB First Run"
+# If the user has not defined a specific password they'd prefer to use for the
+# MySQL root account, generate a 32 digit random new password using the following
+# options:
+#   -s: Generate completely random passwords
+#   -n: Include at least one number in the password
+#   -B: Don't include ambiguous characters in the password
+#   -c: Include at least one capital letter in the password
+
+if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+    export MYSQL_ROOT_PASSWORD="$(pwgen -snBc 32 1)"
+    # log the newly generated MySQL root password to the Docker logs so the user
+    # can look it up for initial provisioning of the server
+    echo "Generated MySQL Root Password: \"$MYSQL_ROOT_PASSWORD\""
+    echo "For security reasons, please log into MySQL and change this password \
+        immediately."
+else
+    echo "Using user-defined password for the MySQL root account."
+fi
+
+# set the mysql root password
+
