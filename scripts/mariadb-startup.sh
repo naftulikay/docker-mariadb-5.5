2301e124f6e3db52a0f1a053739e15daea278e63
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2014-05-18 19:11:38 -0700

    Wrote the initial start script. First run script isn't working because of problem in inotify, if file doesn't exist and we're watching for it, it fails to run, breaks the loop, all is lost.

--- scripts/mariadb-startup.sh
+++ scripts/mariadb-startup.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "MariaDB."
+# if something fails, exit 
+set -e
+
+# if there's no defined timezone, use UTC
+if [ -z "$MYSQL_TIMEZONE" ]; then
+    MYSQL_TIMEZONE="GST-0"
+fi
+
+# if this is the first run, execute the first run script and then 
+# remove it.
+if [ -f "/usr/local/bin/mariadb-first-run" ]; then
+    exec /usr/local/bin/mariadb-first-run && \
+        rm /usr/local/bin/mariadb-first-run
+fi
+
+# startup mariadb as user 'mysql' with the given timezone
+sudo -u mysql mysqld_safe --timezone="$MYSQL_TIMEZONE"
